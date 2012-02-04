bf2fc042994604034838b56aacb6be40d90c1101
Gokul Das <gokuldas.b@gmail.com>
2012-02-04 13:49:07 +0530

    Changed display method for future animation Added license GPL v3 Added .hgignore file

--- parray.py
+++ parray.py
@@ -1,3 +1,25 @@
+###############################################################################
+#    PARRAY - Script to simulate phased arrays in numpy
+#    Copyright (C) 2012 - Gokul Das B
+#
+#                   GNU General Public License - V3
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+"""Module for simulating phased arrays in numpy"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -51,16 +73,18 @@ class PhasedArray:
             xi, yi = np.meshgrid(xt, yt)
             r = np.sqrt(xi ** 2 + yi ** 2)
             # self._mask = np.ma.mask_or(self._mask, r < 12)
-            #val = (az / r ** 2) * np.sin(r + pz)
+            #val = (az / r ** 2) * np.sin(r + pz)    # Implement inverse square
             val = az * np.sin(r + pz)
             self._z = self._z + val
         
     def display(self):
-        """Diesplay field"""
+        """Display field"""
         #zm = np.ma.masked_array(z, mask = self._mask)
-        plt.contourf(self._x, self._y, self._z)
-        plt.plot(self.el_x, self.el_y, 'o')
-        plt.show()
+        fig = plt.figure()
+        ax = fig.add_axes([0, 0, 1, 1])
+        ax.contourf(self._x, self._y, self._z)
+        ax.plot(self.el_x, self.el_y, 'o')
+        fig.show()
 
     def simulate(self):
         """Run phased array simulation"""
